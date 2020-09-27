029fdf6822c2c1f9d73c082c31333c0e48a813c9
Josiah Fowler <josiah.james.fowler@gmail.com>
2020-09-27 05:54:27 -0400

    another one

---
+++ Fundamentals/fillingAnArray.js
@@ -0,0 +1,21 @@
+function arr(N) {
+  let filledArr = [];
+  if (typeof N === "number") {
+    filledArr.push(0);
+    for (i = 1; i < N; i++) {
+      filledArr.push(filledArr[i - 1] + 1);
+    }
+  }
+  return filledArr;
+}
+
+console.log(arr(4));
+console.log(arr(5));
+console.log(arr());
+
+// Testcase
+// describe('#arr creates a new array with the numbers 0 to N-1',_=>{
+//     it('should return an array',_=>Test.expect(arr() instanceof Array));
+//     it('should return a blank array when called with no argument',_=>Test.assertSimilar(arr(), []));
+//     it('should return 0 to 3 when called with 4',_=>Test.assertSimilar(arr(4), [0,1,2,3]));
+//   });
