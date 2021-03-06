37a27048fcf9be952e69c31c588eb5bc5cd7150e
Josiah Fowler <josiah.james.fowler@gmail.com>
2021-03-05 21:21:30 -0500

    a1

---
+++ 8kyu/sumMixedArray.js
@@ -0,0 +1,20 @@
+// Problem: 
+// Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+// Return your answer as a number.
+
+function sumMix(x){
+    let res = 0;
+    let arr = x.forEach((el) => {
+       res += Number(el)
+   })
+    return res;
+  }
+
+
+//   Testcase:
+//   Test.describe("Example tests",_=>{
+//     Test.assertEquals(sumMix([9, 3, '7', '3']), 22);
+//     Test.assertEquals(sumMix(['5', '0', 9, 3, 2, 1, '9', 6, 7]), 42); 
+//     Test.assertEquals(sumMix(['3', 6, 6, 0, '5', 8, 5, '6', 2,'0']), 41);
+//     });
