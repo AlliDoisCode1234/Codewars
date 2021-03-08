be0a6f0dd6e10d660cf3912e1e9c005461c56c4d
Josiah Fowler <josiah.james.fowler@gmail.com>
2021-03-08 05:57:10 -0500

    one

---
+++ 8kyu/nameShuffler.js
@@ -0,0 +1,12 @@
+// Problem:
+// Write a function that returns a string in which firstname is swapped with last name.
+
+// nameShuffler('john McClane'); => "McClane john"
+
+
+function nameShuffler(str){
+    return str.split(" ").reverse().join(' ')
+  }
+
+//   Testcase:
+// Test.assertEquals(nameShuffler('john McClane'),'McClane john');
