5812e41ebecdacbe7143835ae5d7c4315e90202f
Josiah Fowler <josiah.james.fowler@gmail.com>
2021-03-12 09:59:44 -0500

    twleve

---
+++ 8kyu/5withoutNumbers.js
@@ -0,0 +1,17 @@
+// Problem: 
+// Write a function that always returns 5
+
+// Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+// Good luck :)
+
+function unusualFive() {
+    let arr = ['', '', '', '', '']
+    return arr.length
+  }
+
+  describe("unusualFive", function(){
+    it("should return 5", function(){
+      assert.strictEqual(unusualFive(), 5);
+    });
+  });
