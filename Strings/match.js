d337094b26f3c4127cd1ced1eed14ddffdb28f5b
Josiah <josiah.james.fowler@gmail.com>
2021-06-01 22:26:03 -0400

    methods

---
+++ Strings/match.js
@@ -0,0 +1,37 @@
+String.prototype.match()
+
+// The match() method retrieves the result of 
+// matching a string against a regular expression.
+
+
+
+
+// 
+// Example 1
+// 
+
+
+const paragraph = 'The quick brown fox jumps over the lazy dog. It barked.';
+const regex = /[A-Z]/g;
+const found = paragraph.match(regex);
+
+console.log(found);
+// expected output: Array ["T", "I"]
+
+
+// 
+// Example 2
+// 
+
+match(regexp)
+
+// 
+// Example 3
+// 
+
+const str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+const regexp = /[A-E]/gi;
+const matches_array = str.match(regexp);
+
+console.log(matches_array);
+// ['A', 'B', 'C', 'D', 'E', 'a', 'b', 'c', 'd', 'e']
