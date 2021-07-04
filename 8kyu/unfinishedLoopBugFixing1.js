8ff48575f01611b5149c060920694e83d9aec93c
Josiah <josiah.james.fowler@gmail.com>
2021-07-03 22:03:09 -0400

    unfinishedLoop

---
+++ 8kyu/unfinishedLoopBugFixing1.js
@@ -0,0 +1,9 @@
+function createArray(number){
+    let newArray = [];
+    
+    for(let counter = 1; counter <= number; counter++){
+      newArray.push(counter);
+    }
+    
+    return newArray;
+  }
