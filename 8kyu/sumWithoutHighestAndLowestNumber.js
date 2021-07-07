02e5045ab4743e8797229b817750347f7251ab4b
Josiah <josiah.james.fowler@gmail.com>
2021-07-06 21:50:06 -0400

    sumWithoutHighandLow

---
+++ 8kyu/sumWithoutHighestAndLowestNumber.js
@@ -0,0 +1,7 @@
+function sumArray(array) {
+    if (!Array.isArray(array)) return 0
+   return array
+       .sort((a, b) => a - b)
+       .slice(1, array.length - 1)
+       .reduce((a, b) => a + b, 0)
+   }
