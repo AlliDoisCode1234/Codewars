93f681d9c9524815ce9bb48bf2966a0e7cb59ede
Josiah <josiah.james.fowler@gmail.com>
2021-07-15 06:37:37 -0400

    third Angle of a triangle

---
+++ 8kyu/thirdAngleOfATriangle.js
@@ -0,0 +1,9 @@
+const otherAngle = (a, b) => 180 - (a + b)
+//   P: numbers representing interior angles of a triangle
+//   R: Return 3rd angle
+//   E: 30, 60 => 90
+//   E: 10, 40 => 130
+//   E: 10, 20 => 150
+//   R: return the difference of 180 - (angle1 + angle2)
+  
+
