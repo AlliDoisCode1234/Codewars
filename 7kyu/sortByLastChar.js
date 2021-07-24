d1d6f6885e4f0eeb2589268179b6cac9a00346d0
Josiah <josiah.james.fowler@gmail.com>
2021-07-24 12:34:12 -0400

    sort by last char

---
+++ 7kyu/sortByLastChar.js
@@ -0,0 +1,14 @@
+const last = x =>
+//   p: string
+//   R: array of sorted strings by last letter
+  
+//   P: split words into array,
+    // sort by last letter
+  
+  
+  x
+    .split(' ')
+    .sort((a,b) => b[b.length -1] < a[a.length -1])
+  
+  
+   
