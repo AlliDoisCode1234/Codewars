125a1c141d464afb245a24015b2cc9a8a98593ff
Josiah <josiah.james.fowler@gmail.com>
2021-07-25 06:39:04 -0400

    format words into a sentence

---
+++ 6kyu/formatWordsIntoASentence.js
@@ -0,0 +1,11 @@
+// function formatWords(words){
+//   return !words.length || words.forEach(el => el === '') ? '' : words.join(', ')
+// }
+
+function formatWords(words) {
+    if (!words) return "";
+    words = words.filter(v => v.length > 0);
+    if (words.length === 0) return "";
+    if (words.length === 1) return words[0];
+    return words.slice(0, -1).join(", ") + " and " + words.slice(-1);
+  }
