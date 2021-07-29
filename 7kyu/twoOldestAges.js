7c057b3ada75c45c1b741542821c159ee3d9b43b
Josiah <josiah.james.fowler@gmail.com>
2021-07-29 17:15:27 -0400

    two oldest ages

---
+++ 7kyu/twoOldestAges.js
@@ -0,0 +1,6 @@
+// return the two oldest/oldest ages within the array of ages passed in.
+function twoOldestAges(ages){
+    let sorted = ages.sort((a,b) => a - b)
+    return [sorted[sorted.length - 2], sorted[sorted.length - 1]]
+  }
+  
