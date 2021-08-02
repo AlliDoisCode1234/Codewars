45c12fda2a8c02837b9d915ad00f447e9f9b93c6
Josiah <josiah.james.fowler@gmail.com>
2021-08-02 16:28:46 -0400

    lost number refactored

--- 7kyu/lostNumberInNumberSequence.js
+++ 7kyu/lostNumberInNumberSequence.js
@@ -1,12 +1,14 @@
-function findDeletedNumber(arr, mixArr) {
-    let sorted = mixArr.sort((a,b) => a - b)
-    if(sorted.length === arr.length && sorted.every((val, idx) => val === arr[idx])) {
-      return 0
-    } else {
-      for (let i = 0; i <= sorted.length; i++) {
-        if(sorted[i] !== (i + 1)){
-          return (i + 1)
-        }
-      }
-    }
-  }
+// function findDeletedNumber(arr, mixArr) {
+//     let sorted = mixArr.sort((a,b) => a - b)
+//     if(sorted.length === arr.length && sorted.every((val, idx) => val === arr[idx])) {
+//       return 0
+//     } else {
+//       for (let i = 0; i <= sorted.length; i++) {
+//         if(sorted[i] !== (i + 1)){
+//           return (i + 1)
+//         }
+//       }
+//     }
+//   }
+
+const findDeletedNumber = (arr, mixArr) => arr.filter(el=> mixArr.indexOf(el) == -1)[0] || 0
