c330285ec93db7b152db8c9bd31cd2a8a479ca11
Josiah <josiah.james.fowler@gmail.com>
2021-08-02 16:22:31 -0400

    lost number in number sequence

---
+++ 7kyu/lostNumberInNumberSequence.js
@@ -0,0 +1,12 @@
+function findDeletedNumber(arr, mixArr) {
+    let sorted = mixArr.sort((a,b) => a - b)
+    if(sorted.length === arr.length && sorted.every((val, idx) => val === arr[idx])) {
+      return 0
+    } else {
+      for (let i = 0; i <= sorted.length; i++) {
+        if(sorted[i] !== (i + 1)){
+          return (i + 1)
+        }
+      }
+    }
+  }
