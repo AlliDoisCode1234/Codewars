62a04827c41507020cf143c7238716a374f0f2cf
Josiah <josiah.james.fowler@gmail.com>
2021-08-05 18:09:08 -0400

    refactored

--- 7kyu/absentVowel.js
+++ 7kyu/absentVowel.js
@@ -1,29 +1,32 @@
-function absentVowel(x){
-    let charFreq ={
-      'a': 0,
-      'e': 0,
-      'i': 0,
-      'o': 0,
-      'u': 0
-    }
+// function absentVowel(x){
+//     let charFreq ={
+//       'a': 0,
+//       'e': 0,
+//       'i': 0,
+//       'o': 0,
+//       'u': 0
+//     }
     
-    let check = x.split('').map(el => {if(el.toLowerCase() in charFreq ){
-      charFreq[el] += 1
-    }})
+//     let check = x.split('').map(el => {if(el.toLowerCase() in charFreq ){
+//       charFreq[el] += 1
+//     }})
     
-    for (const [key, value] of Object.entries(charFreq)){
-      if(value === 0){
-        if(key === 'a'){
-          return 0
-        } else if( key === 'e'){
-          return 1
-        } else if(key === 'i'){
-          return 2
-        } else if(key === 'o'){
-          return 3
-        } else {
-          return 4
-        }
-      }
-    }
-  }
+//     for (const [key, value] of Object.entries(charFreq)){
+//       if(value === 0){
+//         if(key === 'a'){
+//           return 0
+//         } else if( key === 'e'){
+//           return 1
+//         } else if(key === 'i'){
+//           return 2
+//         } else if(key === 'o'){
+//           return 3
+//         } else {
+//           return 4
+//         }
+//       }
+//     }
+//   }
+
+const absentVowel = string => 
+[...'aeiou'].findIndex(letter => !String.includes(letter))
